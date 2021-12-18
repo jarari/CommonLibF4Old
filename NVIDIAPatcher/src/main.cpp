41a399e8dfc57cdedc1232e7f3744e455b347f55
jarari <hazardhax11@gmail.com>
2021-12-18 04:23:35 -0800

    Initial release

---
+++ NVIDIAPatcher/src/main.cpp
@@ -0,0 +1,88 @@
+#include "PatternScanner.h"
+
+char tempbuf[8192] = { 0 };
+char* _MESSAGE(const char* fmt, ...) {
+	va_list args;
+
+	va_start(args, fmt);
+	vsnprintf(tempbuf, sizeof(tempbuf), fmt, args);
+	va_end(args);
+	spdlog::log(spdlog::level::warn, tempbuf);
+
+	return tempbuf;
+}
+
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface* a_f4se, F4SE::PluginInfo* a_info)
+{
+#ifndef NDEBUG
+	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+#else
+	auto path = logger::log_directory();
+	if (!path) {
+		return false;
+	}
+
+	*path /= fmt::format(FMT_STRING("{}.log"), Version::PROJECT);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+#endif
+
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+
+#ifndef NDEBUG
+	log->set_level(spdlog::level::trace);
+#else
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::warn);
+#endif
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+
+	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
+
+	a_info->infoVersion = F4SE::PluginInfo::kVersion;
+	a_info->name = Version::PROJECT.data();
+	a_info->version = Version::MAJOR;
+
+	if (a_f4se->IsEditor()) {
+		logger::critical("loaded in editor"sv);
+		return false;
+	}
+
+	const auto ver = a_f4se->RuntimeVersion();
+	if (ver < F4SE::RUNTIME_1_10_162) {
+		logger::critical(FMT_STRING("unsupported runtime v{}"), ver.string());
+		return false;
+	}
+
+	return true;
+}
+
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_f4se)
+{
+	F4SE::Init(a_f4se);
+	const F4SE::MessagingInterface* message = F4SE::GetMessagingInterface();
+	message->RegisterListener([](F4SE::MessagingInterface::Message* msg) -> void {
+		if (msg->type == F4SE::MessagingInterface::kGameLoaded) {
+			MemoryRegion* mr = new MemoryRegion();
+			PatternScanner::GetModuleMemoryRegion(mr, "nvwgf2umx.dll");
+
+			if (mr->size > 0) {
+				_MESSAGE("nvwgf2umx.dll Found.");
+				uintptr_t addr = PatternScanner::PatternScanInternal(mr, std::vector<BYTE>{ 0x48, 0x8b, 0x43, 0x50, 0x4c, 0x8b, 0x48, 0x08 }) + 0xB;
+				if (addr) {
+					uint8_t old = *(uint8_t*)addr;
+					REL::safe_write<uint8_t>(addr, 0xEB);
+					_MESSAGE("Patched address %llx. Old : %02X New : %02X", addr, old, 0xEB);
+				}
+				else {
+					_MESSAGE("Pattern not found.");
+				}
+			}
+
+			delete(mr);
+		}
+	});
+
+	return true;
+}
