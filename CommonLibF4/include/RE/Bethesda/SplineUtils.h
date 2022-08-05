0d01313d3b8e5bd2bfc1ccbaeb1a6367e3a478b7
jarari <hazardhax11@gmail.com>
2022-08-05 17:19:50 +0900

    Added SplineUtils::ConnectSpline

--- CommonLibF4/include/RE/Bethesda/SplineUtils.h
+++ CommonLibF4/include/RE/Bethesda/SplineUtils.h
@@ -12,5 +12,11 @@ namespace RE
 			REL::Relocation<func_t> func{ REL::ID(750682) };
 			return func(a_spline);
 		}
+
+		inline void ConnectSpline(TESObjectREFR* akEndpoint1, int32_t linkType1, TESObjectREFR* akEndpoint2, int32_t linkType2, TESObjectREFR* akWireRef) {
+			using func_t = decltype(&ConnectSpline);
+			REL::Relocation<func_t> func{ REL::ID(59311) };
+			return func(akEndpoint1, linkType1, akEndpoint2, linkType2, akWireRef);
+		}
 	}
 }
