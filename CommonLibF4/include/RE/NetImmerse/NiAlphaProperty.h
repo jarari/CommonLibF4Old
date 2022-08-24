fc3492939a09c160a8dfdda40ca8fd90112e44d9
jarari <hazardhax11@gmail.com>
2022-08-25 01:27:09 +0900

    Added functions for setting up NiAlphaProperty

--- CommonLibF4/include/RE/NetImmerse/NiAlphaProperty.h
+++ CommonLibF4/include/RE/NetImmerse/NiAlphaProperty.h
@@ -6,12 +6,25 @@
 namespace RE
 {
 	class __declspec(novtable) NiAlphaProperty :
-		NiProperty  // 00
+		public NiProperty  // 00
 	{
 	public:
 		static constexpr auto RTTI{ RTTI::NiAlphaProperty };
 		static constexpr auto VTABLE{ VTABLE::NiAlphaProperty };
 		static constexpr auto Ni_RTTI{ Ni_RTTI::NiAlphaProperty };
+
+		NiAlphaProperty() {
+			stl::emplace_vtable(this);
+			NiTFlags<std::uint16_t, NiProperty> f;
+			f.flags = 0xEC;
+			flags = f;
+			alphaTestRef = 0;
+		}
+		virtual ~NiAlphaProperty();
+
+		virtual std::int32_t Type() {
+			return 0;
+		}
 
 		enum class AlphaFunction
 		{
@@ -40,9 +53,41 @@ namespace RE
 			kNever
 		};
 
+		void SetDestBlendMode(AlphaFunction f) {
+			using func_t = decltype(&NiAlphaProperty::SetDestBlendMode);
+			REL::Relocation<func_t> func{ REL::ID(725249) };
+			return func(this, f);
+		}
+
+		void SetSrcBlendMode(AlphaFunction f) {
+			using func_t = decltype(&NiAlphaProperty::SetSrcBlendMode);
+			REL::Relocation<func_t> func{ REL::ID(976961) };
+			return func(this, f);
+		}
+
+		void SetTestMode(TestFunction f) {
+			using func_t = decltype(&NiAlphaProperty::SetTestMode);
+			REL::Relocation<func_t> func{ REL::ID(976961), 0x20 };
+			return func(this, f);
+		}
+
+		void SetAlphaBlending(bool b) {
+			using func_t = decltype(&NiAlphaProperty::SetAlphaBlending);
+			REL::Relocation<func_t> func{ REL::ID(645586) };
+			return func(this, b);
+		}
+
+		void SetAlphaTesting(bool b) {
+			using func_t = decltype(&NiAlphaProperty::SetAlphaTesting);
+			REL::Relocation<func_t> func{ REL::ID(645586), 0x20 };
+			return func(this, b);
+		}
+
 		// members
 		NiTFlags<std::uint16_t, NiProperty> flags;  // 28
 		std::int8_t alphaTestRef;                   // 2A
+
+		F4_HEAP_REDEFINE_ALIGNED_NEW(NiAlphaProperty);
 	};
 	static_assert(sizeof(NiAlphaProperty) == 0x30);
 }
--- CommonLibF4/include/RE/NetImmerse/NiProperty.h
+++ CommonLibF4/include/RE/NetImmerse/NiProperty.h
@@ -15,7 +15,7 @@ namespace RE
 		static constexpr auto Ni_RTTI{ Ni_RTTI::NiProperty };
 
 		// add
-		virtual std::int32_t Type() const = 0;            // 28
+		virtual std::int32_t Type() = 0;            // 28
 		virtual void Update(NiUpdateData& a_updateData);  // 29
 	};
 	static_assert(sizeof(NiProperty) == 0x28);
