8fedd89f74ce6f0f927bc0802f9885d387c1e9a4
jarari <hazardhax11@gmail.com>
2023-01-21 03:40:19 -0800

    They should be virtual

--- CommonLibF4/include/RE/NetImmerse/NiCollisionObject.h
+++ CommonLibF4/include/RE/NetImmerse/NiCollisionObject.h
@@ -13,10 +13,10 @@ namespace RE
 		static constexpr auto Ni_RTTI{ Ni_RTTI::NiCollisionObject };
 
 		// add
-		void SetSceneGraphObject(NiAVObject* a_sceneObject);                                               // 28
-		void UpdateWorldData(NiUpdateData& a_updateData);                                                  // 29
-		void Initialize([[maybe_unused]] void* a_data) { return; }                                         // 2A
-		void Convert([[maybe_unused]] std::uint32_t a_version, [[maybe_unused]] void* a_data) { return; }  // 2B
+		virtual void SetSceneGraphObject(NiAVObject* a_sceneObject);                                               // 28
+		virtual void UpdateWorldData(NiUpdateData& a_updateData);                                                  // 29
+		virtual void Initialize([[maybe_unused]] void* a_data) { return; }                                         // 2A
+		virtual void Convert([[maybe_unused]] std::uint32_t a_version, [[maybe_unused]] void* a_data) { return; }  // 2B
 
 		// members
 		NiAVObject* sceneObject;  // 10
